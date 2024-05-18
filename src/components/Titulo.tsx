25151f2edf25857867c7ed9e0aec4a2c482e79b4
Lucas Gabriel <lucas.shirabayashi@gmail.com>
2024-05-17 23:27:34 -0300

    feat: add Layout component

---
+++ src/components/Titulo.tsx
@@ -0,0 +1,14 @@
+interface TituloProps {
+  children: any
+}
+
+const Titulo = (props: TituloProps) => {
+  return (
+    <div className="flex flex-col justify-center">
+      <h1 className="flex justify-center items-center text-2xl py-2 font-bold">{props.children}</h1>
+      <hr className="border-2 border-purple-500" />
+    </div>
+  );
+}
+ 
+export default Titulo;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Layout from "@/components/Layout";
+
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center bg-gradient-to-r  from-purple-300 to-blue-600">
-      <span className="text-4xl">Hello World!</span>
+    <div className="flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500 text-white">
+      <Layout titulo="Cadastro Simples">
+        <span>Conteudo Children</span>
+      </Layout>
     </div>
   );
 }
